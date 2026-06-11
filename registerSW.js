bf47caa4cc5ce6e49f5cf51583a6b482ab070754
statick88 <statick88@users.noreply.github.com>
2026-06-11 17:42:49 +0000

    deploy: 90e73a2e7583577d62678853fcabaa73cabf3a26

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/showroom-digital-inmobiliario/sw.js', { scope: '/showroom-digital-inmobiliario/' })})}
